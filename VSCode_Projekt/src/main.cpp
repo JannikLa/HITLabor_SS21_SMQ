9ce7ca893d01aa9d04febfa02429e178c7d6a366
JoNeckel <63593196+JoNeckel@users.noreply.github.com>
2021-04-28 09:31:28 +0200

    Add files via upload

---
+++ VSCode_Projekt/src/main.cpp
@@ -0,0 +1,14 @@
+//-----------------------------------------------
+// Program: Swimming Pixel Lightshow mit MQTT (ESP32)
+// Karlsruhe University of Applied Science
+// Authors SS21: Jannik Lassen (laja1024), Johannes Neckel (nejo1017)
+// Authors WS20: Jin Yun Ng, Ping Wen Liew, Tim Schroh, Benedikt Ochs (Extended version from Lukas Reimold and Patrick Rodinger)
+// Current Status: test (08.04.2021)
+//------------------------------------------------
+
+/*
+To make it easy to upload every program to the 16 ESP32s, all codes are in seperate .h files.
+The only thing that needs to be changed for the different pixels is the include here in the main.cpp file
+*/
+
+#include "pixel_1_1.h"
